0377c0e881780acc358bd378e3a202a4219f64c8
Kyle Bryenton <kyle.bryenton@gmail.com>
2025-05-09 02:01:11 -0300

    ProcessLM26Extract now appends PWD to top of file

--- ProcessLM26Extract.sh
+++ ProcessLM26Extract.sh
@@ -39,34 +39,36 @@ if [ $# == 0 ]; then
 fi
 
 # Process the files passed as arguments
-(
-for arg in "$@"; do
-    # Make sure the file exists
-    if [ ! -f "$arg" ]; then
-        echo "ERROR: File '$arg' not found." >&2
-        continue
-    fi
-
-    # Extract filename without the extension
-    # Removes the leading './' if the user used a find
-    # Removes "extract_full" if it was left in, but keeps if renamed
-    echo -n "${arg%.result}   " \
-      | sed "s/extract_full//" \
-      | sed "s|^\./||"
-
-    # Extract the MAE and ME values using grep and awk
-    grep "MAE " "$arg" | awk '{printf "%s  ", $NF}'
-    grep "ME "  "$arg" | awk '{printf "%s  ", $NF}'
-
-    # Print the result on a new line
-    echo
-done
-) \
-  | sed "s/\// /g" \
-  | awk '{ 
-      for (i=1 ; i<=NF ; i++) {
-          printf "%-12s  ", $i
-      }
-      printf "\n"
-    }' \
-  > ProcessLM26Extract.dat
+{
+    echo "$PWD"
+    (
+    for arg in "$@"; do
+        # Make sure the file exists
+        if [ ! -f "$arg" ]; then
+            echo "ERROR: File '$arg' not found." >&2
+            continue
+        fi
+    
+        # Extract filename without the extension
+        # Removes the leading './' if the user used a find
+        # Removes "extract_full" if it was left in, but keeps if renamed
+        echo -n "${arg%.result}   " \
+          | sed "s/extract_full//" \
+          | sed "s|^\./||"
+    
+        # Extract the MAE and ME values using grep and awk
+        grep "MAE " "$arg" | awk '{printf "%s  ", $NF}'
+        grep "ME "  "$arg" | awk '{printf "%s  ", $NF}'
+    
+        # Print the result on a new line
+        echo
+    done
+    ) \
+      | sed "s|/| |g" \
+      | awk '{ 
+          for (i=1 ; i<=NF ; i++) {
+              printf "%-12s  ", $i
+          }
+          printf "\n"
+        }' 
+} > ProcessLM26Extract.dat
